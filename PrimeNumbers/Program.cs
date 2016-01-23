98bba043e892b3f3d779d0b42fe380802f5be35c
RakhmetovKuanysh <kuanish-sims@mail.ru>
2016-01-23 14:48:25 +0600

    First version

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        /// <summary>
+        /// A Function for checking prime number
+        /// </summary>
+        /// <param name="s"> /// Convert string to numbers </param>
+        /// <returns></returns>
+        static bool IsPrime(string s)
+        {
+            int x = int.Parse(s);
+            /// Counter for identifiyng prime number
+            int cnt = 0;
+            for (int j = 2; j <= Math.Sqrt(x); ++j)
+            {
+                if (x % j == 0)
+                {
+                    cnt++;
+                }
+            }
+            /// If counter equals 0 or entered number doesn't equal 1 the number is prime
+            return cnt == 0 && x != 1;
+
+        }
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; ++i) {
+                if (IsPrime(args[i]))
+                {
+                    /// Add "is prime"
+                    Console.WriteLine(args[i] + " Is Prime");
+                }
+                else
+                {
+                    Console.WriteLine(args[i] + " Is not Prime");
+                }
+            }
+        }
+    }
+}
---
+++ PrimeNumbers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("PrimeNumbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrimeNumbers")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("79219368-8c96-42c5-801a-c11131709e46")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
